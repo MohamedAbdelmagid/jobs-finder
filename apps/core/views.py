5fcd83f2344ba4f8a07016a3192e150d905f3a4a
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2020-11-29 13:41:29 -0600

    Implement Signup

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
     return render(request, 'core/index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+        
+    context = { 'form': form }
+    return render(request, 'core/signup.html', context)
